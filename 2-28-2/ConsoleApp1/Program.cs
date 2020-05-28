562ae9b21d5f8b012935878cf7bb48026d75d1b1
Jason1Qi <284430723@qq.com>
2020-05-29 02:07:22 +0800

    2月28日作业

---
+++ 2-28-2/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp4
+
+{
+
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[100];
+            Random random = new Random();
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = Convert.ToInt32(random.Next()%100);
+            }
+            foreach(int i in a)
+            {
+                Console.Write(i+"  ");
+            }
+            int max = a[0];
+            int min = a[0];
+            int sum = 0;
+            float ave = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] > max)
+                    max = a[i];
+                if (a[i] < min)
+                    min = a[i];
+                sum += a[i];
+            }
+            float s = sum;
+            ave = s / 100;
+            Console.WriteLine("");
+            Console.WriteLine(max.ToString() + "  " + min.ToString() + "  " +
+                sum.ToString() + "  " + ave.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2-28-3/2-28-3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_28_3
+{
+    class Program
+    {
+        //用“埃氏筛法”求2~ 100以内的素数。2~ 100以内的数，先去掉2的倍数，再去掉3的倍数，再
+        //去掉4的倍数，以此类推...最后剩下的就是素数。
+        static void Main(string[] args)
+        {
+            ArrayList array = new ArrayList();
+            int[] divisor = new int[9];
+            for(int i=2;i<=100;i++)
+            {
+                array.Add(i);
+                if (i <= 10)
+                    divisor[i-2] = i;
+            }
+            foreach(int i in divisor)
+            {
+                for(int j=0;j<array.Capacity;j++)
+                {
+                    if(Convert.ToInt32(array[j])%i==0)
+                        array.RemoveAt(j);
+                    array.TrimToSize();
+                }
+            }
+            foreach(int i in array)
+            Console.Write(i+"   ");
+            Console.ReadLine();
+        }
+    }
+}
+
